bd1d7f746119c2dcaf377e97496e25221b4a1f23
Renata Bandeira <renatabandeiradasilva@gmail.com>
2023-12-06 11:00:30 -0300

    Ajustando texto section about

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,26 +5,8 @@ export default function Footer() {
     return (
         <footer id='information'>
            
-            <p><span>Rê Bandeira</span> &copy; 2022</p>
+            <p><span>Rê Bandeira</span> &copy; 2023</p>
         </footer>
 
     )
 }
-/*
-import {AiFillMail, AiFillEnvironment} from 'react-icons/ai'
-const informations = [
-    {name: 'Localização', id: 'localizacao', key: 'localizacao', info: 'Porto Alegre - RS', icon: <AiFillEnvironment/>},
-    {name: 'e-mail', id: 'email', key:'email',info: 'renatabandeiradasilva@gmail.com', icon: <AiFillMail/>}
-];
- <div className='info-cards'>
-                {informations.map((information) => (
-                    <div className='info-card' key={information.key}>
-                        <span id={information.id}>
-                            {information.icon}
-                        </span>
-                        <h3>{information.name}</h3>
-                        <p>{information.info}</p>
-                    </div>
-                ))}
-            </div>
-*/
--- src/pages/Sobre/index.tsx
+++ src/pages/Sobre/index.tsx
@@ -11,10 +11,13 @@ export default function Sobre(): JSX.Element {
             <main className="about-conteudo">
                 <h1 className="about-title">Olá, sou a Renata;</h1>
                 <article className="about-text">
-                    <p>Iniciei meus estudos  em tecnologia com foco em desenvolvimento web. Atualmente sou graduanda em Engenharia de Software.</p>
-                    <p>Tenho conhecimento e prática em  JavaScript, HTML, CSS e algumas bibliotecas de Nodejs.</p>
-                    <p>Estou aprofundando meus conhecimentos em React, Nodejs, Typescript  e Figma no momento.</p>
-                    <p>Estou disponível para contato através das redes abaixo.</p>
+                    
+                    <p>Atuo profissionalmente com desenvolvimento de software desde o início de 2023. Sou graduanda em Engenharia de Software e tenho curiosidade por diversas áreas dentro da tecnologia. </p>
+                    <p>Dentre as linguagens e ferramentas que possuo conhecimento e experiência se destacam JavaScript,
+                 React, C#, Node.js, SQL e Git</p>
+                 <p>Atualmente tenho buscado estudar mais conceitos e melhores práticas aplicaveis na camada de Back End.</p><br />
+
+                 <p>Estou disponível para contato através das redes abaixo.</p> 
                 </article>                
             </main>   
             <aside className="aside">
